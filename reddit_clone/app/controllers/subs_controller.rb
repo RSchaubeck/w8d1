cf3257e09dd9bfb2eb3a85d0a457315bf1fc24d2
Richard Schaubeck <richard.schaubeck@gmail.com>
2020-03-09 13:05:36 -0400

    added controller logic to subs for update/edit

--- reddit_clone/app/controllers/subs_controller.rb
+++ reddit_clone/app/controllers/subs_controller.rb
@@ -1,11 +1,30 @@
 class SubsController < ApplicationController
 
-    def update
+    before_action :is_moderator?, only: [:edit, :update]
 
+    def update
+        @sub = Sub.find(params[:id])
+        if @sub.update(sub_params)
+            redirect_to sub_url(@sub)
+        else
+            flash.now[:errors] = @sub.errors.full_messages
+            render :edit
+        end
     end
 
     def edit
+        @sub = Sub.find(params[:id])
+    end
 
+    def is_moderator?
+        return true if current_user.subs.find(params[:id])
+        false
+    end
+
+    private
+
+    def sub_params
+        params.require(:sub).permit(:name, :description)
     end
 
 end
