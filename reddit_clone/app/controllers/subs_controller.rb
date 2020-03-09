13998b1a7dba23899646904f69ac2dbee3b1de42
Richard Schaubeck <richard.schaubeck@gmail.com>
2020-03-09 13:55:06 -0400

    added index view and controller logic for the rest of the controller actions

--- reddit_clone/app/controllers/subs_controller.rb
+++ reddit_clone/app/controllers/subs_controller.rb
@@ -2,6 +2,29 @@ class SubsController < ApplicationController
 
     before_action :is_moderator?, only: [:edit, :update]
 
+    def index
+        @subs = Sub.all
+        render :index
+    end
+
+    def show
+        @sub = Sub.find(params[:id])
+    end
+
+    def new
+        @sub = Sub.new
+    end
+
+    def create
+        @sub = current_user.subs.new(sub_params)
+        if @sub.save
+          redirect_to sub_url(@sub)
+        else
+          flash.now[:errors] = @sub.errors.full_messages
+          render :new
+        end
+    end
+
     def update
         @sub = Sub.find(params[:id])
         if @sub.update(sub_params)
