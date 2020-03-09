79bfa792296771803e459c38a007c1e9a9d70ffe
Richard Schaubeck <richard.schaubeck@gmail.com>
2020-03-09 16:05:06 -0400

    added post controller and corresponding actions

---
+++ reddit_clone/app/controllers/posts_controller.rb
@@ -0,0 +1,41 @@
+class PostsController < ApplicationRecord
+    def new
+        @post = Post.new
+        render :new
+    end
+
+    def create
+        @post = current_user.posts.new(post_params)
+        if @post.save
+            redirect_to post_url(@post)
+        else
+            flash.now[:errors] = @post.errors.full_messages
+            render :new
+        end
+    end
+
+    def show
+        @post = Post.find(params[:id])
+        render :show
+    end
+
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def update
+        @post = Post.find(params[:id])
+        if @post.update(post_params)
+            redirect_to post_url(@post)
+        else
+            flash.now[:errors] = @post.errors.full_messages
+            render :edit
+        end
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:url, :title, :content)
+      end
+end
